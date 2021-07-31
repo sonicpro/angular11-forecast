318fcbc924fd260b9bcaad24b65e6a30a35c21c1
Volodymyr Tsurko <chegnenado@yahoo.com>
2021-07-31 15:51:19 +0300

    Fixed unit tests.

--- src/app/forecast/forecast.component.spec.ts
+++ src/app/forecast/forecast.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { ForecastComponent } from './forecast.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('ForecastComponent', () => {
   let component: ForecastComponent;
@@ -8,9 +11,15 @@ describe('ForecastComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ForecastComponent ]
+      declarations: [ ForecastComponent ],
+      imports: [
+        MatSnackBarModule,
+        MatFormFieldModule,
+        MatInputModule,
+        BrowserAnimationsModule
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
